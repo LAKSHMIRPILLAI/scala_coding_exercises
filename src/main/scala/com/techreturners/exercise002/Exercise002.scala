310ed8f37fad4cfcdf9e21f54314b9d41d6971b7
LAKSHMIRPILLAI <luxmi.r.pillai@gmail.com>
2022-02-13 20:26:04 +0000

    Implemented all exercises in  Scala

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,8 +2,16 @@ package com.techreturners.exercise002
 
 class Exercise002 {
 
-  def isFromManchester(/*person: Person*/): Boolean = false
+    def isFromManchester(person: Person): Boolean = person match {
+      case Person(_,_,"Manchester",_)=>true
+      case _ =>false}
 
-  def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = false
+  def canWatchFilm(person: Person, ageLimit: Int ): Boolean = {
+    person.age>=ageLimit
+  }
 
+
+}
+
+case class Person (var firstName: String, var lastName: String, var city: String,var age:Int) {
 }
--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -8,6 +8,14 @@ class Exercise004(var dateTime: LocalDateTime) {
   // https://docs.scala-lang.org/overviews/scala-book/classes-aux-constructors.html
   // Try constructing a "LocalDateTime" from a "LocalDate"
   // You might need to default the time to be something like midnight
+  def getDateTimeWithGigaSecond: LocalDateTime = {
+    import java.time.LocalDate
+    import java.time.format.DateTimeFormatter
+  var processStart:LocalDateTime= dateTime
+  var processEnd:LocalDateTime = processStart .plusSeconds(1000000000)
+  processEnd
 
-  def getDateTimeWithGigaSecond: LocalDateTime = null
+  }
+
+
 }
